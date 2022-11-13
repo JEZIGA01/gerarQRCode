dadd345cb5d5593da8ca3b76e5d08d84fc5851f1
Jéssica Aquiles <73039194+JEZIGA01@users.noreply.github.com>
2022-11-13 17:28:11 -0300

    API GOOGLE CHART

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function GerarQrCode(){
-  let inputSala = document.querySelector('#salaCriada').value = 5;
+  let inputSala = document.querySelector('#salaCriada').value;
   let apiGoogleChart = `https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=`;
   let contentQRCode = apiGoogleChart + inputSala
   document.querySelector('#QRCodeImg').src = contentQRCode;
